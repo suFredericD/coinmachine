e2bbe0d9f7748497f2f2783087524d045a61de4b
suFredericD <ilric6666@gmail.com>
2023-06-13 08:35:26 +0200

    On branch master
    1.Added new logos for socials and tokens
    2.Release web3medias v1
    
    Changes to be committed:
    	modified:   config/css/web3medias.css
    	new file:   media/logos/Crypto__Goku.jpg
    	new file:   media/logos/coin360.png
    	new file:   media/logos/coinmarketcap.jpg
    	new file:   media/logos/cointribune.jpg
    	new file:   media/logos/cryptoMatrix.jpg
    	new file:   media/logos/cryptoast.jpg
    	new file:   media/logos/cryptofarmeur.jpg
    	new file:   media/logos/gac.jpg
    	new file:   media/logos/hasheur.jpg
    	new file:   media/logos/heureka.jpg
    	new file:   media/logos/journalducoin.jpg
    	new file:   media/logos/mrtk.jpg
    	new file:   media/logos/surfinbitcoin.jpg
    	new file:   media/logos/swissborgmania.jpg
    	new file:   media/logos/thebigwhale.jpg
    	new file:   media/logos/tradingview.jpg
    	new file:   media/logos/twitter.jpg
    	new file:   media/tokens/atom.png
    	new file:   media/tokens/bitcoin.png
    	new file:   media/tokens/bnb.png
    	new file:   media/tokens/celo.png
    	new file:   media/tokens/cro.png
    	new file:   media/tokens/elrond.png
    	new file:   media/tokens/ethereum.png
    	new file:   media/tokens/fuse.jpg
    	new file:   media/tokens/gnosis.png
    	new file:   media/tokens/iotx.png
    	new file:   media/tokens/kava.png
    	new file:   media/tokens/moonriver.png
    	new file:   media/tokens/osmosis.jpg
    	new file:   media/tokens/polygon.png
    	new file:   media/tokens/solana.png
    	modified:   pages/web3medias.php
    	modified:   scripts/paging/html_header.php

--- pages/web3medias.php
+++ pages/web3medias.php
@@ -18,10 +18,17 @@ require('..\scripts\paging\html_footer.php');
 require('..\scripts\paging\main_menu.php');
 // Include the database access script
 require('..\admin\db_access.php');
+// Include the database request builder script
+require('..\admin\db_requestBuilder.php');
 
 $fileName = $_SERVER['SCRIPT_NAME'];                    // Get the name of the current script
 $siteInformations = [];
+$tabNewsMediasInfos = getNewsMediasList();
+$tabYoutubeChannelsInfos = getYoutubeChannelsList();
+$tabChartsMediasInfos = getChartsMediasList();
+$tabTwitterInfos = getTwitterList();
 
+// Page building main functions
 createHTMLheader($fileName, $siteInformations);         // Create the HTML header
 createPageheader($fileName);                            // Create the page header
 creatMainMenu($fileName);                               // Create the main menu
@@ -31,7 +38,7 @@ creatMainMenu($fileName);                               // Create the main menu
             <div class="row">
 <!-- Little menu -->
                 <nav id="medias-mini-menu" class="col-2">
-                    <h4>Medias web3</h4>
+                    <h4><a href="#web3-medias-label">Medias web3</a></h4>
                     <ul>
                         <li><a href="#news">Actualités</a></li>
                         <li><a href="#youtube">YouTube</a></li>
@@ -42,28 +49,79 @@ creatMainMenu($fileName);                               // Create the main menu
 <!-- Main content -->
                 <section id="medias-main-content" class="offset-2 col-10">
                     <div id="web3-medias-news" class="row">
-                        <section id="#news" class="col-12">
-                            <h2>Actualités</h2>
-                            
-
+                        <section class="col-12">
+                            <h2><a id="news">Actualités</a></h2>
+<?php   for($i = 0; $i < count($tabNewsMediasInfos); $i++){
+            $strLogo = "../media/logos/" . $tabNewsMediasInfos[$i]['LogoFile'];
+?>
+                            <article class="row">
+                                <img class="col-3" src="<?php echo $strLogo;?>"/>
+                                <div class="col-9">
+                                    <div class="row">
+                                        <h3 class="col-9"><?php echo $tabNewsMediasInfos[$i]['Name'];?></h3>
+                                        <a class="media-url col-9" href="<?php echo $tabNewsMediasInfos[$i]['Url'];?>" target="_blank"><?php echo $tabNewsMediasInfos[$i]['Url'];?><span class="fa-solid fa-arrow-up-right-from-square"></span></a>
+                                    </div>
+                                </div>
+                                <p class="media-desciption offset-1 col-10"><?php echo $tabNewsMediasInfos[$i]['Description'];?></p>
+                            </article>
+<?php   } ?>
                         </section>
                     </div>
                     <div id="web3-medias-youtube" class="row">
                         <section id="#youtube" class="col-12">
-                            <h2>YouTube</h2>
-
+                            <h2><a id="youtube">YouTube</a></h2>
+<?php   for($i = 0; $i < count($tabYoutubeChannelsInfos); $i++){
+            $strLogo = "../media/logos/" . $tabYoutubeChannelsInfos[$i]['LogoFile'];
+?>
+                            <article class="row">
+                                <img class="col-3" src="<?php echo $strLogo;?>"/>
+                                <div class="col-9">
+                                    <div class="row">
+                                        <h3 class="col-9"><?php echo $tabYoutubeChannelsInfos[$i]['Name'];?></h3>
+                                        <a class="media-url col-9" href="<?php echo $tabYoutubeChannelsInfos[$i]['Url'];?>" target="_blank"><?php echo $tabYoutubeChannelsInfos[$i]['Url'];?><span class="fa-solid fa-arrow-up-right-from-square"></span></a>
+                                    </div>
+                                </div>
+                                <p class="media-desciption offset-1 col-10"><?php echo $tabYoutubeChannelsInfos[$i]['Description'];?></p>
+                            </article>
+<?php   } ?>
                         </section>
                     </div>
                     <div id="web3-medias-charts" class="row">
                         <section id="#charts" class="col-12">
-                            <h2>Charts</h2>
-
+                            <h2><a id="charts">Charts</a></h2>
+<?php   for($i = 0; $i < count($tabChartsMediasInfos); $i++){
+            $strLogo = "../media/logos/" . $tabChartsMediasInfos[$i]['LogoFile'];
+?>
+                            <article class="row">
+                                <img class="col-3" src="<?php echo $strLogo;?>"/>
+                                <div class="col-9">
+                                    <div class="row">
+                                        <h3 class="col-9"><?php echo $tabChartsMediasInfos[$i]['Name'];?></h3>
+                                        <a class="media-url col-9" href="<?php echo $tabChartsMediasInfos[$i]['Url'];?>" target="_blank"><?php echo $tabChartsMediasInfos[$i]['Url'];?><span class="fa-solid fa-arrow-up-right-from-square"></span></a>
+                                    </div>
+                                </div>
+                                <p class="media-desciption offset-1 col-10"><?php echo $tabChartsMediasInfos[$i]['Description'];?></p>
+                            </article>
+<?php   } ?>
                         </section>
                     </div>
                     <div id="web3-medias-twitter" class="row">
                         <section id="#twitter" class="col-12">
-                            <h2>Twitter</h2>
-
+                            <h2><a id="twitter">Twitter</a></h2>
+<?php   for($i = 0; $i < count($tabTwitterInfos); $i++){
+            $strLogo = "../media/logos/" . $tabTwitterInfos[$i]['LogoFile'];
+?>
+                            <article class="row">
+                                <img class="col-3" src="<?php echo $strLogo;?>"/>
+                                <div class="col-9">
+                                    <div class="row">
+                                        <h3 class="col-9"><?php echo $tabTwitterInfos[$i]['Name'];?></h3>
+                                        <a class="media-url col-9" href="<?php echo $tabTwitterInfos[$i]['Url'];?>" target="_blank"><?php echo $tabTwitterInfos[$i]['Url'];?><span class="fa-solid fa-arrow-up-right-from-square"></span></a>
+                                    </div>
+                                </div>
+                                <p class="media-desciption offset-1 col-10"><?php echo $tabTwitterInfos[$i]['Description'];?></p>
+                            </article>
+<?php   } ?>
                         </section>
                     </div>
                 </section>
--- scripts/paging/html_header.php
+++ scripts/paging/html_header.php
@@ -41,7 +41,7 @@ function createHTMLheader($fileName, $siteInformations){
     <favicon href="<?php echo $strFavicon;?>" />
     
     <!-- Google fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Frijole|Press+Start+2P|Syncopate|Wallpoet" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Frijole|Share+Tech+Mono|Press+Start+2P|Syncopate|Wallpoet" rel="stylesheet">
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <!-- Attached CSS file -->
