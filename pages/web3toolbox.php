95f604dfa1d2f0fbf225012eb0427e2b2172ba27
suFredericD <ilric6666@gmail.com>
2023-06-14 17:41:57 +0200

    On branch master
    1.Added web3toolbox page + css + js
    2.Added thumbnails for web3toolbox page
    
    Changes to be committed:
    	new file:   config/css/web3toolbox.css
    	new file:   media/thumbnails/ariswap01.png
    	new file:   media/thumbnails/bitfeed-live01.png
    	new file:   media/thumbnails/chainlist01.png
    	new file:   media/thumbnails/debank01.png
    	new file:   media/thumbnails/etherscan01.png
    	new file:   media/thumbnails/lunarcrush01.png
    	new file:   media/thumbnails/omnibridge01.png
    	new file:   media/thumbnails/revoke01.png
    	modified:   pages/web3toolbox.php
    	new file:   scripts/js/web3toolbox.js

--- pages/web3toolbox.php
+++ pages/web3toolbox.php
@@ -6,19 +6,20 @@
 // Role         : known and used blockchains tools
 // Author       : CoinMachine
 // Creation     : 2023-06-12
-// Last update  : 2021-06-12
+// Last update  : 2021-06-14
 // =====================================================================================================
-// Include the HTML header builder
-require('../scripts\paging\html_header.php');
-// Include the page header builder
-require('../scripts\paging\page_header.php');
-// Include the HTML footer builder
-require('../scripts\paging\html_footer.php');
-// Include the menu builder script
-require('../scripts\paging\main_menu.php');
+require('..\scripts\paging\html_header.php');           // Include the HTML header builder
+require('..\scripts\paging\page_header.php');           // Include the page header builder
+require('..\scripts\paging\html_footer.php');           // Include the HTML footer builder
+require('..\scripts\paging\main_menu.php');             // Include the menu builder script
+require('..\admin\db_access.php');                      // Include the database access script
+require('..\admin\db_requestBuilder.php');              // Include the database request builder script
 
 $fileName = $_SERVER['SCRIPT_NAME'];                    // Get the name of the current script
 $siteInformations = [];
+
+$tabToolBoxTypes = getToolBoxTypes();                   // Get the list of the toolbox types
+$tabToolBaxItems = getToolBoxItems();                   // Get the list of the toolbox items
 
 createHTMLheader($fileName, $siteInformations);         // Create the HTML header
 createPageheader($fileName);                            // Create the page header
@@ -26,27 +27,51 @@ creatMainMenu($fileName);                               // Create the main menu
 ?>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        <label id ="toobox-main-label" for="toobox-main" class="col-10">Useful ToolBox</label>
+        <section id ="toobox-main" class="col-11">
+            <div class="row">
+<!-- Little menu -->
+                <nav id="toolbox-mini-menu" class="col-3">
+                        <ul id="toolbox-mini-nav">
+<?php   for($i = 0; $i < count($tabToolBoxTypes); $i++){ ?>
+                            <li id="mini-item-li<?php echo $tabToolBoxTypes[$i]['Id'];?>" class="mini-item">
+                                <a href="#tml<?php echo $tabToolBoxTypes[$i]['Id'];?>"><?php echo $tabToolBoxTypes[$i]['Title'];?></a>
+                            </li>
+<?php   } ?>
+                        </ul>
+                </nav>
+<!-- Main content -->
+                <section id="toolbox-main-content" class="col-9">
+                    <div id="toolbox-main-list" class="row">
+<?php   for($i = 0; $i < count($tabToolBoxTypes); $i++){ ?>
+                        <section id="tml<?php echo $tabToolBoxTypes[$i]['Id'];?>" class="tml-section col-12">
+                            <div class="row">
+<?php       for($j = 0; $j < count($tabToolBaxItems); $j++){
+                if($tabToolBaxItems[$j]['TypeId'] == $tabToolBoxTypes[$i]['Id']){
+                    $strThumbnail = "../media/thumbnails/" . $tabToolBaxItems[$j]['Thumbnail'];
+                    $strLinkTitle = "Visiter le site de " . $tabToolBaxItems[$j]['Name'] . "...";
+?>
+                                <article class="toolbox-item-container col-11">
+                                    <div class="row">
+                                        <a class="col-4" href="<?php echo $tabToolBaxItems[$j]['Url'];?>" title="<?php echo $strLinkTitle;?>">
+                                            <h3><?php echo $tabToolBaxItems[$j]['Name'];?></h3>
+                                        </a>
+                                        <h4 class="col-8"><?php echo $tabToolBaxItems[$j]['Title'];?></h4>
+                                        <a class="col-4" href="<?php echo $tabToolBaxItems[$j]['Url'];?>" title="<?php echo $strLinkTitle;?>">
+                                            <img src="<?php echo $strThumbnail;?>" alt="<?php echo $tabToolBaxItems[$j]['Name'];?> thumbnail" />
+                                        </a>
+                                        <p class="toolbox-text col-8"><?php echo $tabToolBaxItems[$j]['Description'];?></p>
+                                </article>
+<?php           }
+            } ?>    
+                            </div>
+                        </section>
+<?php   } ?>
+                    </div>
+                </section>
+<!-- END content -->
+            </div>
+        </section>
 <?php
 createHTMLfooter($fileName);                            // Create the HTML footer
 ?>
---
+++ scripts/js/web3toolbox.js
@@ -0,0 +1,14 @@
+// =====================================================================================================
+// Project      : CoinMachine platform
+// Context      : blockchain/cryptocurrency education and consulting, IT developement
+// File         : web3toolbox.js
+// Role         : animation JS script for web3toolbox.php
+// Author       : CoinMachine
+// Creation     : 2023-06-14
+// Last update  : 2021-06-14
+// =====================================================================================================
+// ================ DOM ELEMENTS ================ //
+
+// ================ FUNCTIONS ================ //
+
+// ================ EVENT LISTENERS ================ //
