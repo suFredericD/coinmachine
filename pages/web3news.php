8d09af9ef64a5e0ed5f95847e618eb747b3f95aa
suFredericD <ilric6666@gmail.com>
2023-07-13 11:42:56 +0200

    On branch master
    1.Changed news page to display old news in little cards sorted by month
    
    Changes to be committed:
    	modified:   config/css/web3news.css
    	new file:   media/logos/cleanSatMining.png
    	new file:   media/logos/magaliCosmic.jpg
    	new file:   media/logos/meria.png
    	new file:   media/logos/paymium.jpg
    	new file:   media/logos/richardDetente.jpg
    	new file:   media/logos/sebastienGouspillou.jpg
    	new file:   media/logos/woleet.png
    	new file:   media/people/gilesCadignan.jpg
    	new file:   media/people/owenSimonin.png
    	new file:   media/people/pierreNoizat.jpg
    	new file:   media/people/richardDetente.jpeg
    	new file:   media/people/stephanieCabossioras.jpg
    	new file:   media/thumbnails/20min-pierre-noizat-bitcoin-etalon-2023-07-08.png
    	new file:   media/thumbnails/owenSimonin-interview01.png
    	new file:   media/thumbnails/richardDetente-podcast01.png
    	new file:   media/thumbnews/cointribune-hard-fork-indore-polygon-2023-07-12.png
    	new file:   media/thumbnews/cointribune-tether-gele-25-millions-usdt-multichain-2023-07-09.png
    	new file:   media/thumbnews/cryptoast-arkham-intelligence-2023-07-13.png
    	new file:   media/thumbnews/cryptoast-banques-britanniques-panique-livre-numerique-2023-07-08.png
    	new file:   media/thumbnews/cryptoast-web3-leaders-forum-2023-07-10.png
    	new file:   media/thumbnews/journalducoin-UE-data-gouvernance-act-2023-07-05.png
    	new file:   media/thumbnews/journalducoin-arkham-binance-launchpad-2023-07-11.png
    	modified:   pages/web3news.php

--- pages/web3news.php
+++ pages/web3news.php
@@ -6,7 +6,7 @@
 // Role         : known fresh informations about web3
 // Author       : CoinMachine
 // Creation     : 2023-06-24
-// Last update  : 2021-06-30
+// Last update  : 2021-07-13
 // =====================================================================================================
 require('../scripts/paging/html_header.php');           // Include the HTML header builder
 require('../scripts/paging/page_header.php');           // Include the page header builder
@@ -19,6 +19,8 @@ $fileName = $_SERVER['SCRIPT_NAME'];                    // Get the name of the c
 $siteInformations = [];
 
 $tabNewsInfos = getLastThirtyNewsList();                // Get the last 30 news from the database
+$tabMonths = ["", "Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet",
+              "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
 
 // Page building main functions
 createHTMLheader($fileName, $siteInformations);         // Create the HTML header
@@ -32,28 +34,55 @@ creatMainMenu($fileName);                               // Create the main menu
 <!-- Main content -->
                 <section id="web3-news-main-content" class="col-11">
                     <div id="web3-news-list" class="row">
-<?php   foreach ($tabNewsInfos as $newsInfos) {
-            $dateNews = new DateTime($newsInfos['Date']);
-            $strDateNews = "[" . $dateNews->format('d/m/Y') . "] - <span class=\"news-source\">" . $newsInfos['SourceName'] . "</span>";
-            $strImageUrl = "../media/thumbnews/" . $newsInfos['Thumbnail'];
-            $strLogoSource = "../media/logos/" . $newsInfos['SourceLogo'];
-            $strNewsLinkTitle = "Lire l'article complet sur " . $newsInfos['SourceName'] . "...";
+<?php   for($i=0; $i<5; $i++) {
+            $dateNews = new DateTime($tabNewsInfos[$i]['Date']);
+            $strDateNews = "[" . $dateNews->format('d/m/Y') . "] - <span class=\"news-source\">" . $tabNewsInfos[$i]['SourceName'] . "</span>";
+            $strImageUrl = "../media/thumbnews/" . $tabNewsInfos[$i]['Thumbnail'];
+            $strLogoSource = "../media/logos/" . $tabNewsInfos[$i]['SourceLogo'];
+            $strNewsLinkTitle = "Lire l'article complet sur " . $tabNewsInfos[$i]['SourceName'] . "...";
 ?>
-                        <div class="news-container col-12">
+                        <article class="news-container col-12">
                             <div class="row">
-                                <img class="img-news col-xl-4" src="<?php echo $strImageUrl;?>" alt="<?php echo $newsInfos['Title']; ?>">
+                                <img class="img-news col-xl-4" src="<?= $strImageUrl ?>" alt="<?= $tabNewsInfos[$i]['Title'] ?>">
                                 <div class="col-xl-8">
                                     <div class="row">
-                                        <p class="news-date col-10 col-sm-11 col-md-11 col-xl-11"><?php echo $strDateNews; ?></p>
-                                        <img class="news-source-logo col-2 col-sm-1 col-md-1 col-xl-1" src="<?php echo $strLogoSource;?>" alt="<?php echo $newsInfos['SourceName']; ?>">
-                                        <h3 class="news-title col-12"><?php echo $newsInfos['Title'];?></h3>
-                                        <p class="news-text col-12"><?php echo $newsInfos['Text'];?></p>
-                                        <a class="news-link col-12" href="<?php echo $newsInfos['Url'];?>" target="_blank"><span class="fa-solid fa-arrow-up-right-from-square"></span><?php echo $strNewsLinkTitle;?></a>
+                                        <p class="news-date col-10 col-sm-11 col-md-11 col-xl-11"><?= $strDateNews ?></p>
+                                        <img class="news-source-logo col-2 col-sm-1 col-md-1 col-xl-1" src="<?= $strLogoSource?>" alt="<?= $tabNewsInfos[$i]['SourceName'] ?>">
+                                        <h3 class="news-title col-12"><?= $tabNewsInfos[$i]['Title'] ?></h3>
+                                        <p class="news-text col-12"><?= $tabNewsInfos[$i]['Text'] ?></p>
+                                        <a class="news-link col-12" href="<?= $tabNewsInfos[$i]['Url'] ?>" target="_blank"><span class="fa-solid fa-arrow-up-right-from-square"></span><?= $strNewsLinkTitle ?></a>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </article>
 <?php   } ?>
+                        <section id="old-news" class="col-12">
+                            <div id="old-news-row" class="row">
+<?php   $intLastMonth = 0;
+        for($i=5; $i<count($tabNewsInfos); $i++) {
+            $dateNews = new DateTime($tabNewsInfos[$i]['Date']);
+            $strImageUrl = "../media/thumbnews/" . $tabNewsInfos[$i]['Thumbnail'];
+            $strLogoSource = "../media/logos/" . $tabNewsInfos[$i]['SourceLogo'];
+            $strNewsLinkTitleShort = "Article complet";
+            if($intLastMonth == 0 || $intLastMonth != $dateNews->format('m')){
+                $intLastMonth = $dateNews->format('m');
+                $strMonth = $tabMonths[intval($intLastMonth)] . " " . $dateNews->format('Y');
+?>
+                                <h2 class="old-news-month col-12"><?= $strMonth?></h2>
+<?php       }
+?>
+                                <article class="little-card-news col-2">
+                                    <div class="row">
+                                        <div class="little-date-news col-9 col-sm-9 col-md-9 col-lg-9 col-xl-9"><?= "[" . $dateNews->format('d/m/Y') . "]" ?></div>
+                                        <img class="little-news-source-logo col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3" src="<?= $strLogoSource?>" alt="<?= $tabNewsInfos[$i]['SourceName'] ?>">
+                                        <img class="img-news col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12" src="<?= $strImageUrl ?>" alt="<?= $tabNewsInfos[$i]['Title'] ?>">
+                                        <div class="little-title-news col-12"><?= $tabNewsInfos[$i]['Title'] ?></div>
+                                        <a class="little-link-news col-12" href="<?= $tabNewsInfos[$i]['Url'] ?>" target="_blank"><span class="fa-solid fa-arrow-up-right-from-square"></span><?= $strNewsLinkTitleShort ?></a>
+                                    </div>
+                                </article>
+<?php   } ?>
+                            </div>
+                        </section>
                     </div>
                 </section>
             </div>
